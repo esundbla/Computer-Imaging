b0667b215596b649cf70d1296e746e483dc257d5
esundbla <69823637+esundbla@users.noreply.github.com>
2021-09-10 08:15:26 -0600

    Finalized Code working cv.2

--- Assignment_1BGR.py
+++ Assignment_1BGR.py
@@ -10,10 +10,12 @@ import math
 
 windowname = 'me'
 img = cv2.imread('prof pic.jpg')
-LENGTH, WIDTH, COLORS = img.shape
+LENGTH, WIDTH, COLORS = img.shape   # global variables of imported pic
 
 
 def ellipseFunction(x, y):
+    """ helper function takes a pixel location as a co-ordinate and returns
+        the value of the equation (x-h)^2/a^2 + (y-k)^2/b^2 the ellipse equation"""
     h = LENGTH/2
     k = WIDTH/3
     a = ((x-k)**2)/((WIDTH/3)**2)
@@ -24,11 +26,11 @@ def ellipseFunction(x, y):
 for i in range(LENGTH):
     for j in range(WIDTH):
         val = ellipseFunction(j, i)
-        if val > 1.0:
-            img[i, j, 0] = 255 - ((val - 1) * 50)
-            img[i, j, 1] = 255 - ((val - 1) * 50)
-            img[i, j, 2] = 255 - ((val - 1) * 50)
-
+        if val > 1.0:                       # if return value of a given pixel is >1 the pixel isn't inside the ellipse
+            img[i, j, 0] = 255 - ((val - 1) * 50)   # using 255-max val given by ellipse we can gradient out
+            img[i, j, 1] = 255 - ((val - 1) * 50)   # the exterior of the ellipse to created a fake grayscale
+            img[i, j, 2] = 255 - ((val - 1) * 50)   # 50 hard coded in/could be found by entering 0,0 pixel in
+                                                    # ellipseFunc and dividing 255 by given val
 
 cv2.imshow(windowname, img)
 print(LENGTH, WIDTH)
--- Assignment_1GRAY.py
+++ Assignment_1GRAY.py
@@ -1,5 +1,5 @@
 """
-Assignment 1 Test program takes input pic pup in grayscale
+Assignment 1 Test program takes input pic in grayscale
 generates an elliptical portrait with gradient grayscale exterior
 """
 
@@ -11,12 +11,14 @@ import math
 windowname = 'pup'
 img = cv2.imread('prof pic.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-LENGTH, WIDTH = img.shape
+LENGTH, WIDTH = img.shape   # global variables of imported pic
 
 
 def ellipseFunction(x, y):
-    h = 528//2
-    k = 528//2
+    """ helper function takes a pixel location as a co-ordinate and returns
+    the value of the equation (x-h)^2/a^2 + (y-k)^2/b^2 the ellipse equation"""
+    h = LENGTH//2
+    k = WIDTH//2
     a = ((x-h)**2)/((h/2)**2)
     b = ((y-k)**2)/(((3*k)/4)**2)
     return a+b
@@ -26,10 +28,11 @@ for i in range(LENGTH):
     for j in range(WIDTH):
         val = ellipseFunction(j, i)
         print(val)
-        if val > 1.0:
-            img[i, j] = 255 - ((val - 1) * 50)
-            img[i, j] = 255 - ((val - 1) * 50)
-            img[i, j] = 255 - ((val - 1) * 50)
+        if val > 1.0:            # if return value of a given pixel is >1 the pixel isnt inside the ellipse
+            img[i, j] = 255 - ((val - 1) * 50)  # using 0-max val given by ellipse we can gradient out
+            img[i, j] = 255 - ((val - 1) * 50)  # the exterior of the ellipse to created a fake grayscale
+            img[i, j] = 255 - ((val - 1) * 50)  # 50 hard coded in/could be found by entering 0,0 pixel in ellipseFunc
+                                                # and dividing 255 by given val
 
 
 cv2.imshow(windowname, img)
