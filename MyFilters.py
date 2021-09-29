4c21e93adcb28c29a6b0d744e7a9f570f534db4a
Erik <esundbla@msudenver.edu>
2021-09-29 13:47:51 -0600

    Fillter

---
+++ MyFilters.py
@@ -0,0 +1,20 @@
+""" Define The filter function to take the kernal and name and output the resutls"""
+def filter(pic, kernal, name):
+""" Generic Filter func takes pic, filter kernal, and filter name"""
+
+def average(pic):
+
+def sobel(pic):
+
+def laplacian(pic):
+
+def median(pic):
+
+def gaussiab(pic):
+
+def bonus1(pic):
+
+def bonus2(pic):
+
+def pupilEdge(iris):
+
