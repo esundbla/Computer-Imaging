320b1724ed524ee9b4ea5aa70f793b34b1fd45c8
Erik <esundbla@msudenver.edu>
2021-09-29 14:10:33 -0600

    Fillter + Demo

--- MyFilters.py
+++ MyFilters.py
@@ -1,20 +1,16 @@
+import numpy
+import cv2
+import
 """ Define The filter function to take the kernal and name and output the resutls"""
+
+global_filters = {
+    "Average":  ,
+    "Sobel": ,
+    "Laplacian": ,
+    "Median": ,
+    "Gaussian": ,
+    "Bonus1": ,
+    "Bonus2":     }
+
 def filter(pic, kernal, name):
 """ Generic Filter func takes pic, filter kernal, and filter name"""
-
-def average(pic):
-
-def sobel(pic):
-
-def laplacian(pic):
-
-def median(pic):
-
-def gaussiab(pic):
-
-def bonus1(pic):
-
-def bonus2(pic):
-
-def pupilEdge(iris):
-
