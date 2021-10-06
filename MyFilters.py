74aabb5d7e5527a9c1bb684de413825bb5aa212c
Erik <esundbla@msudenver.edu>
2021-10-06 12:45:03 -0600

    Finalized

--- MyFilters.py
+++ MyFilters.py
@@ -1,9 +1,19 @@
+"""
+Erik Sundblad
+CS3150
+10/6/2021
+My Filters is a program to demonstrate the implementation of a number of image filtering techniques
+"""
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from scipy.signal import convolve2d
+import math
 
-global_filters = {
+
+
+global_filters = {          #Utilizing a python Dict. to store multiple kerels and there respective names
     "Average": np.array([[(1/49), (1/49), (1/49), (1/49), (1/49), (1/49), (1/49)],
                         [(1/49), (1/49), (1/49), (1/49), (1/49), (1/49), (1/49)],
                         [(1/49), (1/49), (1/49), (1/49), (1/49), (1/49), (1/49)],
@@ -39,7 +49,7 @@ global_filters = {
                         [(4/382), (11/382), (25/382), (50/382), (25/382), (11/382), (4/382)],
                         [(3/382), (7/382), (14/382), (25/382), (14/382), (7/382), (3/382)],
                         [(2/382), (4/382), (7/382), (11/382), (7/382), (4/382), (2/382)],
-                        [(1/382), (2/382), (3/382), (4/382), (3/382), (2/382), (1/382)]]),}
+                        [(1/382), (2/382), (3/382), (4/382), (3/382), (2/382), (1/382)]])}
 
 
 def filter(pic, kernel):
@@ -56,11 +66,13 @@ def gradientEdge(pic):
     """ Gradient edge function combines the verticle and horizontal sobel filters """
     ver = cv2.filter2D(pic, -1, global_filters["Sobel(V)"])
     hor = cv2.filter2D(pic, -1, global_filters["Sobel(H)"])
-    #v2 = np.square(ver)
+    """ Following block was to aquire gradient edge through distance formula
+        had dificulty plotting inside code the array values changed to unusable data type
+    v2 = np.square(ver)
     #h2 = np.square(hor)
     #g2 = np.add(v2, h2)
-    #gradient = np.sqrt(g2)
-    gradient = ver + hor
+    #gradient = np.sqrt(g2)"""
+    gradient = ver + hor    #Used summation of two filters as close approximate of gradient
     plt.figure()
     plt.title('Gradient Edge')
     plt.imshow(gradient, cmap='gray', vmin=0, vmax=255)
@@ -69,8 +81,8 @@ def gradientEdge(pic):
 
 
 def median(pic):
-    """ Median filter need special implementation """
-    height, width, something = np.shape(pic)
+    """ Median filter need special implementation using a dynamic kernel """
+    height, width = np.shape(pic)
     med = np.ones((height, width), dtype=float)
     for i in range(1, height - 2):
         for j in range(1, width - 2):
@@ -89,32 +101,42 @@ if __name__ == "__main__":
     plt.title('Source Pic')
     plt.imshow(pic, cmap='gray', vmin=0, vmax=255)
     plt.show()
-    #filter(pic, "Average")
-    #filter(pic, "Sobel(V)")
-    #filter(pic, "Sobel(H)")
-    #gradientEdge(pic)
-    #median(pic)
-    #filter(pic, "Laplacian")
-    #filter(pic, "Gaussian")
-    #filter(pic, "Self_Def_Gaus")
-    #filter(pic, "Spacial-large")
+    filter(pic, "Average")
+    filter(pic, "Sobel(V)")
+    filter(pic, "Sobel(H)")
+    gradientEdge(pic)
+    median(pic)
+    filter(pic, "Laplacian")
+    filter(pic, "Gaussian")
+    filter(pic, "Self_Def_Gaus")
+    filter(pic, "Spacial-large")
 
+    #Code to handle multiple filtering the Iris img
     pic2 = cv2.imread('Iris.bmp')
     detect = filter((filter((filter(filter(pic2, 'Gaussian'), 'Laplacian') * pic2), 'Average') - pic2), 'Laplacian')
+    detect = filter(filter(filter(detect, 'Gaussian'), 'Gaussian'), 'Spacial-large')
     plt.figure()
     plt.title('Original')
     plt.imshow(pic2, cmap='gray', vmin=0, vmax=255)
     plt.show()
     plt.figure()
-    plt.title('Twice filtered')
-    plt.imshow(detect, cmap='gray', vmin=0, vmax=255)
+    plt.title('Pupil location')
+    plt.imshow(detect - pic2, cmap='gray', vmin=0, vmax=255)
     plt.show()
 
 
-
-
-
-
-
-
-
+    #This was code for an expiremental pattern match of the pupil
+    """
+    patern_blank = np.full((140, 140), 0)
+    h, w = np.shape(patern_blank)
+    for i in range(h):
+        for j in range(w):
+            distance = math.sqrt(((i-70)**2) + ((j-70)**2))
+            if distance < 17.5:
+                patern_blank[i, j] = 255
+    plt.figure()
+    plt.title('Original')
+    plt.imshow(patern_blank, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+    global_filters.update({"patern_match": patern_blank}) #Code to update the filter dictionary 
+    filter(detect, "patern_match")"""
