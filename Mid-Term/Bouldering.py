1aa39ef3b25bdfe4511f1e97a68827a29a500570
Erik <esundbla@msudenver.edu>
2021-11-08 13:34:23 -0700

    outlined almost complete

--- Mid-Term/Bouldering.py
+++ Mid-Term/Bouldering.py
@@ -4,6 +4,19 @@ import cv2
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
+
+global_filters = {          #Utilizing a python Dict. to store multiple kerels and there respective names
+    "Sobel(V)": np.array([
+         [-1.0, 0.0, 1.0]
+        ,[-2.0, 0.0, 2.0]
+        ,[-1.0, 0.0, 1.0]
+        ]),
+    "Sobel(H)": np.array([
+         [-1.0, -2.0, -1.0]
+        ,[0.0, 0.0, 0.0]
+        ,[1.0, 2.0, 1.0]
+        ])}
 
 
 #Given color thresholds
@@ -47,15 +60,50 @@ def findPath(img, color):
     path_raw = cv2.bitwise_and(img, img, mask=color_mask)
     #show(path_raw, 'RGB')
     morphed = morphTool(path_raw)
+    luv = cv2.cvtColor(morphed, cv2.COLOR_BGR2LUV)
+    l = luv[:, :, 0]
+    edge = gradientEdge(l)
+    dial_edge = morphTool2(edge)
+    length, width = dial_edge.shape
+    for i in range(length):
+        for j in range(width):
+            if dial_edge[i,j] > 100:
+                dial_edge[i,j] = 128
+            else:
+                dial_edge[i,j] = 0
+    show(dial_edge, 'gray')
+    img_luv = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+    img_luv[:,:, 0] = img_luv[:,:, 0] - (dial_edge)
+    path_final = cv2.cvtColor(img_luv, cv2.COLOR_LUV2BGR)
+    show(path_final, 'RGB')
 
 def morphTool(img):
-    kernel_1 = np.ones((27, 27), np.uint8)  # square kernal of 1
+    kernel_1 = np.ones((35, 35), np.uint8)  # square kernal of 1
     kernel_2 = np.ones((19, 19), np.uint8)  # square kernal of 1
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel_2)
     #show(opening, 'RGB')
     dilation = cv2.dilate(opening, kernel_1, iterations = 4)
-    show(dilation, 'RGB')
+    #show(dilation, 'RGB')
     return dilation
+
+def gradientEdge(pic):
+    """ Gradient edge function combines the verticle and horizontal sobel filters """
+    ver = convolve2d(pic, global_filters["Sobel(V)"], mode='same', boundary='symm', fillvalue=0)
+    hor = convolve2d(pic, global_filters["Sobel(H)"] , mode='same', boundary='symm', fillvalue=0)
+    gradient = np.sqrt(np.square(ver) + np.square(hor))
+    gradient *= 255.0/np.max(gradient)
+    plt.figure()
+    plt.title('Gradient Edge')
+    plt.imshow(gradient, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+    return gradient
+
+def morphTool2(img):
+    kernel = np.ones((35, 35), np.uint8)  # square kernal of 1
+    dilation = cv2.dilate(img, kernel, iterations=1)
+    show(dilation, 'gray')
+    return dilation
+
 
 
 
