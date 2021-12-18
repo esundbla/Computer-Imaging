01bb417db64e77300bdb54efd88015dbea54862a
esundbla <69823637+esundbla@users.noreply.github.com>
2021-12-17 19:33:14 -0700

    Final Project completed

--- Final_Project/Hair_Color_Mod.py
+++ Final_Project/Hair_Color_Mod.py
@@ -1,3 +1,4 @@
+import random
 import cv2 as cv
 import numpy as np
 from scipy.signal import convolve2d
@@ -17,14 +18,29 @@ global_filters = {          # Kernels for gradient edge filter
         ])}
 
 
-def morphTool(img):
-    """"""
-    kernel_1 = np.ones((11, 11), np.uint8)  # square kernal of 1
-    kernel_2 = np.ones((10, 10), np.uint8)  # square kernal of 2
+def pixel_perc_filter(perc, hair_space):
+    length, width = hair_space.shape
+    percent_filt = np.ones((length, width), np.uint8)
+    for i in range(length):
+        for j in range(width):
+            if hair_space[i, j] > 0 and i < 1200:
+                percent_filt[i, j] = (0.5+ (perc/100))
+    return percent_filt
 
-    open = cv.morphologyEx(img, cv.MORPH_OPEN, kernel_2)
-    close = cv.morphologyEx(open, cv.MORPH_CLOSE, kernel_1)
-    return close
+
+def morphTool(img):
+
+    kernel_1 = np.ones((17, 17), np.uint8)
+    kernel_2 = np.ones((25, 25), np.uint8)
+    kernel_3 = np.ones((37, 37), np.uint8)
+    kernel_4 = np.ones((3, 3), np.uint8)
+
+    dilation = cv.dilate(img, kernel_1, iterations=1)
+    open = cv.morphologyEx(dilation, cv.MORPH_OPEN, kernel_2)
+    erosion = cv.erode(open, kernel_3, iterations=2)
+    open2 = cv.morphologyEx(erosion, cv.MORPH_OPEN, kernel_2)
+    d2 = cv.dilate(open2, kernel_4, iterations=33)
+    return d2
 
 
 def gradientEdge(pic):
@@ -39,63 +55,55 @@ if __name__ == "__main__":
     initial_img = cv.imread('Charlotte(Crop).jpg')
     length, width, extra = initial_img.shape
     rgb_img = cv.cvtColor(initial_img, cv.COLOR_BGR2RGB)
+
     plt.figure()
-    plt.title('initial color')
+    plt.title('original')
     plt.imshow(rgb_img)
-    #rgb_img[:, :, 0] = 0 #Red
-    rgb_img[:, :, 1] = 0 #Green
-    rgb_img[:, :, 2] = 0 #Blue
-    plt.figure()
-    plt.title('rgb')
-    plt.imshow(rgb_img)
-    plt.show()
 
-    gray_img = cv.cvtColor(initial_img, cv.COLOR_BGR2GRAY)
-    gradient = gradientEdge(gray_img)
-    plt.figure()
-    plt.title('gradient')
-    plt.imshow(gradient)
-    plt.show()
 
-    morph2 = morphTool(gradient)
-    plt.figure()
-    plt.title('gradient')
-    plt.imshow(morph2)
-    plt.show()
-
-    """red_img = rgb_img[:, :, 0]
-    morph = morphTool(red_img)
-    #morph = morphTool(morph)
-    #morph = morphTool(morph)
-    plt.figure()
-    plt.title('gradient')
-    plt.imshow(morph)
-    plt.show()
-
-    for i in range(length):
-        for j in range(width):
-            if red_img[i, j] > 150 or red_img[i, j] < 70:
-                red_img[i, j] = 0
-    plt.figure()
-    plt.title('rgb altered space')
-    plt.imshow(red_img)
-    plt.show()
-    
-
+    red_img = rgb_img[:, :, 0]
+    blue_img = rgb_img[:, :, 2]
+    green_img = rgb_img[:, :, 1]
 
     luv = cv.cvtColor(initial_img, cv.COLOR_BGR2LUV)
     lum = luv[:, :, 0]
-    plt.figure()
-    plt.title('initial color')
-    plt.imshow(lum)
 
+    grad = gradientEdge(lum)
     for i in range(length):
         for j in range(width):
-            if lum[i,j] > 100:
-                lum[i,j] = 0
+            if grad[i, j] > 11:
+                grad[i, j] = 255
+            else:
+                grad[i, j] = 0
+
+
+    morph_grad = morphTool(grad)
+    #plt.figure()
+    #plt.title('luv grad morph')
+    #plt.imshow(morph_grad)
+    #plt.show()
+
+
+    red_per = float(input("Red Percent: "))
+    new_red = pixel_perc_filter(red_per, morph_grad)
+    grn_per = float(input("Green Percent: "))
+    new_green = pixel_perc_filter(grn_per, morph_grad)
+    blu_per = float(input("Blue Percent: "))
+    new_blue = pixel_perc_filter(blu_per, morph_grad)
+    mod_img = rgb_img
+    mod_img[:, :, 0] = (mod_img[:, :, 0] * new_red)
+    mod_img[:, :, 1] = (mod_img[:, :, 1] * new_green)
+    mod_img[:, :, 2] = (mod_img[:, :, 2] * new_blue)
+
     plt.figure()
     plt.title('modified')
-    plt.imshow(lum)
-    plt.show()"""
+    plt.imshow(mod_img)
+    plt.show()
+    exit()
+
+
+
+
+
 
 
